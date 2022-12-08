3aeec69f5cf04b9bb94faf25752ab0960bde9192
jiaji wei <jiajiwei76@gmail.com>
2022-12-08 17:11:41 +0800

    [feat] add unit test

--- contracts/Disperse.sol
+++ contracts/Disperse.sol
@@ -1,109 +1,82 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.17;
 
 interface IERC20 {
     function transfer(address to, uint256 value) external returns (bool);
 
-    function transferFrom(
-        address from,
-        address to,
-        uint256 value
-    ) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
 
 interface IERC721_IERC1155 {
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) external;
+    function safeTransferFrom(address from, address to, uint256 tokenId) external;
 
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        uint256 amount,
-        bytes calldata data
-    ) external;
+    function safeTransferFrom(address from, address to, uint256 tokenId, uint256 amount, bytes calldata data)
+        external;
 }
 
 contract Disperse {
+    using SafeERC20 for ERC20;
+
     function disperseNFT(
         address recipient,
         IERC721_IERC1155[] calldata tokens,
         uint256[] calldata tokenIds,
         uint256[] calldata amounts,
         bytes calldata data
-    ) external {
+    )
+        external
+    {
         for (uint256 index; index < tokenIds.length; index++) {
             if (amounts[index] > 0) {
-                tokens[index].safeTransferFrom(
-                    msg.sender,
-                    recipient,
-                    tokenIds[index],
-                    amounts[index],
-                    data
-                );
+                tokens[index].safeTransferFrom(msg.sender, recipient, tokenIds[index], amounts[index], data);
             } else {
-                tokens[index].safeTransferFrom(
-                    msg.sender,
-                    recipient,
-                    tokenIds[index]
-                );
+                tokens[index].safeTransferFrom(msg.sender, recipient, tokenIds[index]);
             }
         }
     }
 
-    function disperseEther(address[] memory recipients, uint256[] memory values)
-        external
-        payable
-    {
-        for (uint256 i = 0; i < recipients.length; i++)
+    function disperseEther(address[] memory recipients, uint256[] memory values) external payable {
+        for (uint256 i = 0; i < recipients.length; i++) {
             payable(recipients[i]).transfer(values[i]);
+        }
         uint256 balance = address(this).balance;
-        if (balance > 0) payable(msg.sender).transfer(balance);
+        if (balance > 0) {
+            payable(msg.sender).transfer(balance);
+        }
     }
 
-    function disperseToken(
-        IERC20 token,
-        address[] memory recipients,
-        uint256[] memory values
-    ) external {
+    function disperseToken(IERC20 token, address[] memory recipients, uint256[] memory values) external {
         uint256 total = 0;
-        for (uint256 i = 0; i < recipients.length; i++) total += values[i];
+        for (uint256 i = 0; i < recipients.length; i++) {
+            total += values[i];
+        }
         require(token.transferFrom(msg.sender, address(this), total));
-        for (uint256 i = 0; i < recipients.length; i++)
+        for (uint256 i = 0; i < recipients.length; i++) {
             require(token.transfer(recipients[i], values[i]));
+        }
     }
 
-    function disperseTokenSimple(
-        IERC20 token,
-        address[] memory recipients,
-        uint256[] memory values
-    ) external {
-        for (uint256 i = 0; i < recipients.length; i++)
+    function disperseTokenSimple(IERC20 token, address[] memory recipients, uint256[] memory values) external {
+        for (uint256 i = 0; i < recipients.length; i++) {
             require(token.transferFrom(msg.sender, recipients[i], values[i]));
+        }
     }
 
-    function disperseEtherSameValue(address[] memory recipients, uint256 value)
-        external
-        payable
-    {
-        for (uint256 i = 0; i < recipients.length; i++)
+    function disperseEtherSameValue(address[] memory recipients, uint256 value) external payable {
+        for (uint256 i = 0; i < recipients.length; i++) {
             payable(recipients[i]).transfer(value);
+        }
         uint256 balance = address(this).balance;
-        if (balance > 0) payable(msg.sender).transfer(balance);
+        if (balance > 0) {
+            payable(msg.sender).transfer(balance);
+        }
     }
 
-    function disperseTokenSameValue(
-        IERC20 token,
-        address[] memory recipients,
-        uint256 value
-    ) external {
+    function disperseTokenSameValue(IERC20 token, address[] memory recipients, uint256 value) external {
         uint256 total = value * recipients.length;
         require(token.transferFrom(msg.sender, address(this), total));
-        for (uint256 i = 0; i < recipients.length; i++)
+        for (uint256 i = 0; i < recipients.length; i++) {
             require(token.transfer(recipients[i], value));
+        }
     }
 }
--- test/Disperse.t.sol
+++ test/Disperse.t.sol
@@ -2,53 +2,176 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+
 import "../contracts/Disperse.sol";
 
+import {Utilities} from "./utils/Utilities.sol";
+import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 import {MockERC721} from "solmate/test/utils/mocks/MockERC721.sol";
+import {MockERC1155} from "solmate/test/utils/mocks/MockERC1155.sol";
 
-contract CounterTest is Test {
+contract DisperseTest is Test {
     Disperse public disperse;
 
+    MockERC20 public token;
+    MockERC721 public nft_1;
+    MockERC721 public nft_2;
+    MockERC1155 public nft1155_1;
+    MockERC1155 public nft1155_2;
+
+    Utilities internal utils;
+    address[] internal users;
 
     function setUp() public {
+        utils = new Utilities();
         disperse = new Disperse();
 
+        token = new MockERC20("Token", "TKN", 18);
+        nft_1 = new MockERC721("NFT1", "NFT1");
+        nft_2 = new MockERC721("NFT2", "NFT2");
+        nft1155_1 = new MockERC1155();
+        nft1155_2 = new MockERC1155();
+
+        users = utils.createUsers(5);
+
+        token.mint(address(this), 10000 ether);
+
+        for (uint256 i = 0; i < 5; i++) {
+            nft_1.mint(address(this), i);
+            nft_2.mint(address(this), i);
+        }
+
+        uint256[] memory ids = new uint256[](3);
+        ids[0] = 0;
+        ids[1] = 1;
+        ids[2] = 2;
+        uint256[] memory amounts = new uint256[](3);
+        amounts[0] = 10;
+        amounts[1] = 10;
+        amounts[2] = 10;
+
+        nft1155_1.batchMint(address(this), ids, amounts, "");
+        nft1155_2.batchMint(address(this), ids, amounts, "");
+
+        vm.startPrank(address(this));
+        token.approve(address(disperse), 10000 ether);
+        nft_1.setApprovalForAll(address(disperse), true);
+        nft_2.setApprovalForAll(address(disperse), true);
+        nft1155_1.setApprovalForAll(address(disperse), true);
+        nft1155_2.setApprovalForAll(address(disperse), true);
     }
 
-    function testDisperseNFT() public {
+    function testDisperseNFT_721() public {
+        IERC721_IERC1155[] memory tokens = new IERC721_IERC1155[](2);
+        tokens[0] = IERC721_IERC1155(address(nft_1));
+        tokens[1] = IERC721_IERC1155(address(nft_1));
+
+        uint256[] memory tokenIds = new uint256[](2);
+        tokenIds[0] = 1;
+        tokenIds[1] = 2;
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = 0;
+        amounts[1] = 0;
+
+        disperse.disperseNFT(address(1), tokens, tokenIds, amounts, "");
+
+        assertEq(nft_1.ownerOf(1), address(1));
+        assertEq(nft_1.ownerOf(2), address(1));
     }
-    function disperseNFT(
-        address recipient,
-        IERC721_IERC1155[] calldata tokens,
-        uint256[] calldata tokenIds,
-        uint256[] calldata amounts,
-        bytes calldata data
-    )
-        external
-    {}
+
+    function testDisperseNFT_1155() public {
+        IERC721_IERC1155[] memory tokens = new IERC721_IERC1155[](2);
+        tokens[0] = IERC721_IERC1155(address(nft1155_1));
+        tokens[1] = IERC721_IERC1155(address(nft1155_1));
+
+        uint256[] memory tokenIds = new uint256[](2);
+        tokenIds[0] = 1;
+        tokenIds[1] = 2;
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = 5;
+        amounts[1] = 5;
+
+        disperse.disperseNFT(address(1), tokens, tokenIds, amounts, "");
+
+        assertEq(nft1155_1.balanceOf(address(1), 1), 5);
+        assertEq(nft1155_1.balanceOf(address(1), 2), 5);
+    }
 
     function testDisperseEther() public {
-        // TODO
+        vm.deal(address(this), 100 ether);
+
+        uint256[] memory values = new uint256[](5);
+        values[0] = 1 ether;
+        values[1] = 2 ether;
+        values[2] = 3 ether;
+        values[3] = 4 ether;
+        values[4] = 5 ether;
+
+        disperse.disperseEther{value: 15 ether}(users, values);
+
+        assertEq(users[0].balance, 101 ether); // default balance is 100
+        assertEq(users[1].balance, 102 ether);
+        assertEq(users[2].balance, 103 ether);
+        assertEq(users[3].balance, 104 ether);
+        assertEq(users[4].balance, 105 ether);
     }
-    function disperseEther(address[] memory recipients, uint256[] memory values) external payable {}
 
     function testDisperseToken() public {
-        // TODO
-    }
-    function disperseToken(IERC20 token, address[] memory recipients, uint256[] memory values) external {}
+        uint256[] memory values = new uint256[](5);
+        values[0] = 1 ether;
+        values[1] = 2 ether;
+        values[2] = 3 ether;
+        values[3] = 4 ether;
+        values[4] = 5 ether;
 
-    function testDisperseEtherSimple() public {
-        // TODO
+        disperse.disperseToken(IERC20(address(token)), users, values);
+
+        assertEq(token.balanceOf(users[0]), 1 ether);
+        assertEq(token.balanceOf(users[1]), 2 ether);
+        assertEq(token.balanceOf(users[2]), 3 ether);
+        assertEq(token.balanceOf(users[3]), 4 ether);
+        assertEq(token.balanceOf(users[4]), 5 ether);
     }
+
+    function testDisperseTokenSimple() public {
+     
+
+        disperse.disperseTokenSimple(IERC20(address(token)), users, 1);
+
+        assertEq(token.balanceOf(users[0]), 1 ether);
+        assertEq(token.balanceOf(users[1]), 2 ether);
+        assertEq(token.balanceOf(users[2]), 3 ether);
+        assertEq(token.balanceOf(users[3]), 4 ether);
+        assertEq(token.balanceOf(users[4]), 5 ether);
+    }
+
     function disperseTokenSimple(IERC20 token, address[] memory recipients, uint256[] memory values) external {}
 
-    function testDisperseEtherSameValue() public {
-        // TODO
-    }
-    function disperseEtherSameValue(address[] memory recipients, uint256 value) external payable {}
+    // function testDisperseEtherSameValue() public {
+    //     // TODO
+    // }
+    // function disperseEtherSameValue(address[] memory recipients, uint256 value) external payable {}
 
-    function testDisperseTokenSameValue() public {
-        // TODO
+    // function testDisperseTokenSameValue() public {
+    //     // TODO
+    // }
+    // function disperseTokenSameValue(IERC20 token, address[] memory recipients, uint256 value) external {}
+
+    function onERC1155Received(address, address, uint256, uint256, bytes memory) public virtual returns (bytes4) {
+        return this.onERC1155Received.selector;
     }
-    function disperseTokenSameValue(IERC20 token, address[] memory recipients, uint256 value) external {}
+
+    function onERC1155BatchReceived(address, address, uint256[] memory, uint256[] memory, bytes memory)
+        public
+        virtual
+        returns (bytes4)
+    {
+        return this.onERC1155BatchReceived.selector;
+    }
+
+    function onERC721Received(address, address, uint256, bytes memory) public virtual returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
 }
