93898434ae552eaec3eb3bf51923061fe4c40cd2
jiaji wei <jiajiwei76@gmail.com>
2022-12-08 19:43:15 +0800

    [feat] update readme and deploy scripts

--- script/Disperse.s.sol
+++ script/Disperse.s.sol
@@ -3,10 +3,16 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
-contract DisperseScript is Script {
-    function setUp() public {}
+import "../contracts/Disperse.sol";
 
+contract DisperseScript is Script {
     function run() public {
-        vm.broadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        address disperseContract = address(new Disperse());
+        console.log("disperseContract:", disperseContract);
+
+        vm.stopBroadcast();
     }
 }
